e5fd093777a83df1f0ccbf2ded463cc51fd61ec5
Matt Jarrett <cujarrett@gmail.com>
2019-11-18 20:37:06 -0600

    Adds conditionals

---
+++ conditionals.py
@@ -0,0 +1,10 @@
+x = int(input("Enter an integer:  "))
+if x < 0:
+  x = 0
+  print("Negative number changed to 0.")
+elif x == 0:
+  print("0 entered.")
+elif (x == 1):
+  print("1 entered.")
+else:
+  print("Some other number.")
